deb372f31ea89d3048394a9cf5cd5617ca95399e
Raimon Esteve <raimonesteve@gmail.com>
2022-11-09 09:49:20 +0100

    Use declarative index definition for ModelSQL issue5757

--- party.py
+++ party.py
@@ -11,11 +11,11 @@ class PartyAlternativeReport(ModelSQL, ModelView):
     '''Party Alternative Report'''
     __name__ = 'party.alternative_report'
 
-    party = fields.Many2One('party.party', 'Party', required=True, select=True,
+    party = fields.Many2One('party.party', 'Party', required=True,
         ondelete='CASCADE')
-    model_name = fields.Selection([], 'Model', required=True, select=True)
+    model_name = fields.Selection([], 'Model', required=True)
     report = fields.Many2One('ir.action.report', 'Report', required=True,
-        select=True, ondelete='CASCADE', domain=[
+        ondelete='CASCADE', domain=[
             ('model', '=', Eval('model_name')),
             ], depends=['model_name'])
 
